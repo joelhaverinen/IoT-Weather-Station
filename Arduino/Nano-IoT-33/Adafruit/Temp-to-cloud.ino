794c8c41e72503576e0d86fcf9505124a18d0a8c
Joel Haverinen <44695636+joelhaverinen@users.noreply.github.com>
2025-05-18 16:59:31 +0300

    Update Temp-to-cloud.ino
    Finnish comments translated into English

--- Arduino/Nano-IoT-33/Adafruit/Temp-to-cloud.ino
+++ Arduino/Nano-IoT-33/Adafruit/Temp-to-cloud.ino
@@ -3,24 +3,24 @@
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
 
-// WiFi-tiedot
+// WiFi credentials
 #define WIFI_SSID     "xxxx"
 #define WIFI_PASS     "xxxx"
 
-// Adafruit IO -tiedot
+// Adafruit IO credentials
 #define AIO_SERVER      "io.adafruit.com"
 #define AIO_SERVERPORT  1883
 #define AIO_USERNAME    "xxxx"
 #define AIO_KEY         "xxxx"
 
-// MQTT-asiakas
+// MQTT client
 WiFiClient client;
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
 
-// Lämpötila feed
+// Temperature feed
 Adafruit_MQTT_Publish temperatureFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/temperature");
 
-// DHT-anturi
+// DHT sensor
 #define DHTPIN 2
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
@@ -30,7 +30,7 @@ void setup() {
   delay(10);
   dht.begin();
 
-  // Yhdistä WiFiin
+  // Connect to WiFi
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -40,13 +40,13 @@ void setup() {
 }
 
 void loop() {
-  // Yhdistä MQTT:hen
+  // Connect to MQTT
   if (!mqtt.connected()) {
     reconnect();
   }
   mqtt.processPackets(10000);
   
-  // Lue lämpötila
+  // Read temperature
   float temp = dht.readTemperature();
   if (isnan(temp)) {
     Serial.println("Failed to read from DHT sensor!");
@@ -57,7 +57,7 @@ void loop() {
   Serial.println(temp);
   temperatureFeed.publish(temp);
 
-  delay(60000); // lähetä 60 sek välein
+  delay(60000); // send every 60 seconds
 }
 
 void reconnect() {
